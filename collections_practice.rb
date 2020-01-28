da6fcc516ec3bf79f5f322d992d0c243a719da31
Karen Daniela <kdaniimora@gmail.com>
2020-01-28 19:51:10 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
+def array_asc = [6, 8, 9, 4, 3, ,2, 5, 7, 1]
+array.sort do |a, b|
+  a <=> b 
+end 
 
+def array_desc = []
+end 
+
+def sort_array_char_count = []
+end
+
+def swap_elemnts =[]
+end 
+
+def reverse_array = []
+end 
+
+def kesha_maker = []
+end 
+
+def find_a = []
+end
+
+def sum_array = []
+end
+
+def add_s = []
+end
