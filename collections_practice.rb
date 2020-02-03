e78bfe8ddb40635d52905384e75b4b7b3ae9d7fd
Karen Daniela <kdaniimora@gmail.com>
2020-02-03 19:40:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,27 +1,35 @@
-def array_asc = [6, 8, 9, 4, 3, ,2, 5, 7, 1]
-array.sort do |a, b|
-  a <=> b 
+def array_asc(array)
+array.sort 
 end 
 
-def array_desc = []
+def array_desc(array)
+  array.sort do |right, left|
+    right <=> left
 end 
 
-def sort_array_char_count = []
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.lenght <=> right.lenght
 end
 
-def swap_elemnts =[]
+def swap_elememnts(array)
+  array[1], array[2] <=> array[2], array[1]
+array
 end 
 
-def reverse_array = []
+def reverse_array(array)
+  array.reverse 
 end 
 
-def kesha_maker = []
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
 end 
 
-def find_a = []
+def find_a()
 end
 
-def sum_array = []
+def sum_array()
 end
 
 def add_s = []
